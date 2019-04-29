7941c7dd1a960a8c1d0f44afa14b37c27216f22f
abell <abell@meteorcomm.com>
2019-04-29 15:57:34 -0700

    Building out the admin menu will contiue later today

---
+++ Scripts/AdminMenu.js
@@ -0,0 +1,7 @@
+﻿$(function () {
+    //lines 3-5 open and close a menu for a specifc atrribute
+    $("[data-admin-menu]").hover(function() {
+        $('[data-admin-menu]').toggleClass('open');
+
+    });
+});
